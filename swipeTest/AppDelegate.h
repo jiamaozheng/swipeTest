1f608c12caf0706f9ffb73ba2d3654a49f2ef565
Jiamao Zheng <jiamaoz@yahoo.com>
2015-07-06 22:32:23 -0500

    Initial Commit

---
+++ swipeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  swipeTest
+//
+//  Created by jiamao zheng on 7/6/15.
+//  Copyright (c) 2015 jiamaozheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
